dd02613ed1e9565c8f2f5fbc3e2b13339913b2fe
nihongo-deb <dal34@tpu.ru>
2021-09-19 17:56:08 +0700

    first commit

---
+++ app/src/main/java/nihongo_deb/com/cftvaluteapp/ListViewAdapter.java
@@ -0,0 +1,41 @@
+package nihongo_deb.com.cftvaluteapp;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
+public class ListViewAdapter extends ArrayAdapter<JSONObject> {
+    int listLayout;
+    ArrayList<JSONObject> valutes;
+    Context context;
+
+    public ListViewAdapter(Context context, int listLayout, int field, ArrayList<JSONObject> valutes){
+        super(context, listLayout, field, valutes);
+        this.context = context;
+        this.listLayout = listLayout;
+        this.valutes = valutes;
+    }
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent){
+        LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View listViewItem = inflater.inflate(listLayout, null, false);
+        TextView name = listViewItem.findViewById(R.id.tv_name);
+        TextView value = listViewItem.findViewById(R.id.tv_value);
+        try{
+            name.setText(valutes.get(position).getString("Name"));
+            value.setText(valutes.get(position).getString("Value"));
+        }catch (JSONException je){
+            je.printStackTrace();
+        }
+        return listViewItem;
+    }
+
+}
---
+++ app/src/main/java/nihongo_deb/com/cftvaluteapp/MainActivity.java
@@ -0,0 +1,94 @@
+package nihongo_deb.com.cftvaluteapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class MainActivity extends AppCompatActivity {
+    private static final String JSON_URL = "https://www.cbr-xml-daily.ru/daily_json.js";
+
+    ListView valuteItems;
+
+    @Override
+    protected void onSaveInstanceState(Bundle savedInstanceState) {
+        super.onSaveInstanceState(savedInstanceState);
+        savedInstanceState.putInt("ScrollPosition", valuteItems.getTop());
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        valuteItems.setTop(savedInstanceState.getInt("ScrollPosition", 0));
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        valuteItems = findViewById(R.id.lv_valutes);
+
+        loadJSONFromURL(JSON_URL);
+    }
+
+    private void loadJSONFromURL(String url){
+        final ProgressBar progressBar = findViewById(R.id.pb_loading);
+        progressBar.setVisibility(View.VISIBLE);
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        progressBar.setVisibility(View.INVISIBLE);
+                        try{
+                            JSONObject obj = new JSONObject(response);
+                            ArrayList<JSONObject> listItems = getArrayListFromJSONObject(obj);
+                            ListAdapter adapter = new ListViewAdapter(getApplicationContext(), R.layout.valute_item, R.id.tv_name, listItems);
+                            valuteItems.setAdapter(adapter);
+                        }catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                },
+                new Response.ErrorListener(){
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(getApplicationContext(), error.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                });
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        requestQueue.add(stringRequest);
+    }
+
+    private static ArrayList<JSONObject> getArrayListFromJSONObject(JSONObject jsonObject){
+        ArrayList<JSONObject> joList = new ArrayList<>();
+        try{
+            JSONObject valute = jsonObject.getJSONObject("Valute");
+            Iterator<String> keys = valute.keys();
+            while (keys.hasNext()){
+                String key = keys.next();
+                joList.add(valute.getJSONObject(key));
+            }
+        }catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return joList;
+    }
+
+}
